41256766dca80226c98fe1c4181409009a7459dc
Sabin Shrestha <162944408+sabinshrestha1@users.noreply.github.com>
2026-01-28 10:12:46 +0545

    Clarify base URL for custom domain usage
    Updated base URL comment for clarity regarding custom domain.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/", // because you're using custom domain: skypeak.com.np
 });
